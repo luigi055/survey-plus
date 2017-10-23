56f503dd6baeaf0e4f5ce7734d9749dec5a814e6
Luigi Doganieri <luigi5021@gmail.com>
2017-10-22 23:15:46 -0400

    solving enviroment variables on passport.js

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,4 +1,4 @@
-require ('./../config');
+// require ('./../config');
 const passport = require ('passport');
 const GoogleStrategy = require ('passport-google-oauth20');
 const FacebookStrategy = require ('passport-facebook');
@@ -58,99 +58,99 @@ passport.use (
   )
 );
 
-passport.use (
-  new FacebookStrategy (
-    {
-      clientID: process.env.facebookClientID,
-      clientSecret: process.env.facebookClientSecret,
-      callbackURL: '/auth/facebook/callback',
-      proxy: true,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne ({
-        facebookId: profile.id,
-      })
-        .then (existingUser => {
-          if (!existingUser) {
-            const newUser = new User ({
-              facebookId: profile.id,
-              displayName: profile.displayName,
-              gender: profile.gender,
-              provider: profile.provider,
-            });
+// passport.use (
+//   new FacebookStrategy (
+//     {
+//       clientID: process.env.facebookClientID,
+//       clientSecret: process.env.facebookClientSecret,
+//       callbackURL: '/auth/facebook/callback',
+//       proxy: true,
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       User.findOne ({
+//         facebookId: profile.id,
+//       })
+//         .then (existingUser => {
+//           if (!existingUser) {
+//             const newUser = new User ({
+//               facebookId: profile.id,
+//               displayName: profile.displayName,
+//               gender: profile.gender,
+//               provider: profile.provider,
+//             });
 
-            newUser.save ().then (user => done (null, user));
-          }
+//             newUser.save ().then (user => done (null, user));
+//           }
 
-          // user exists so no error (null) and pass in the user
-          done (null, existingUser);
-        })
-        .catch (err => done (err));
-      // console.log ('profile', profile);
-    }
-  )
-);
+//           // user exists so no error (null) and pass in the user
+//           done (null, existingUser);
+//         })
+//         .catch (err => done (err));
+//       // console.log ('profile', profile);
+//     }
+//   )
+// );
 
-passport.use (
-  new GitHubStrategy (
-    {
-      clientID: process.env.githubClientID,
-      clientSecret: process.env.githubClientSecret,
-      callbackURL: '/auth/github/callback',
-      proxy: true,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne ({
-        githubId: profile.id,
-      })
-        .then (existingUser => {
-          if (!existingUser) {
-            const newUser = new User ({
-              githubId: profile.id,
-              displayName: profile.displayName,
-              avatar: profile.avatar_url,
-              provider: profile.provider,
-            });
+// passport.use (
+//   new GitHubStrategy (
+//     {
+//       clientID: process.env.githubClientID,
+//       clientSecret: process.env.githubClientSecret,
+//       callbackURL: '/auth/github/callback',
+//       proxy: true,
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       User.findOne ({
+//         githubId: profile.id,
+//       })
+//         .then (existingUser => {
+//           if (!existingUser) {
+//             const newUser = new User ({
+//               githubId: profile.id,
+//               displayName: profile.displayName,
+//               avatar: profile.avatar_url,
+//               provider: profile.provider,
+//             });
 
-            newUser.save ().then (user => done (null, user));
-          }
+//             newUser.save ().then (user => done (null, user));
+//           }
 
-          // user exists so no error (null) and pass in the user
-          done (null, existingUser);
-        })
-        .catch (err => done (err));
-    }
-  )
-);
+//           // user exists so no error (null) and pass in the user
+//           done (null, existingUser);
+//         })
+//         .catch (err => done (err));
+//     }
+//   )
+// );
 
-passport.use (
-  new TwitterStrategy (
-    {
-      consumerKey: process.env.twitterConsumerKey,
-      consumerSecret: process.env.twitterConsumerSecret,
-      callbackURL: 'http://localhost:3000/auth/twitter/callback',
-      proxy: true,
-    },
-    function (token, tokenSecret, profile, done) {
-      User.findOne ({
-        twitterId: profile.id,
-      })
-        .then (existingUser => {
-          if (!existingUser) {
-            const newUser = new User ({
-              twitterId: profile.id,
-              displayName: profile.name,
-              avatar: profile.photos[0].value,
-              provider: profile.provider,
-            });
+// passport.use (
+//   new TwitterStrategy (
+//     {
+//       consumerKey: process.env.twitterConsumerKey,
+//       consumerSecret: process.env.twitterConsumerSecret,
+//       callbackURL: 'http://localhost:3000/auth/twitter/callback',
+//       proxy: true,
+//     },
+//     function (token, tokenSecret, profile, done) {
+//       User.findOne ({
+//         twitterId: profile.id,
+//       })
+//         .then (existingUser => {
+//           if (!existingUser) {
+//             const newUser = new User ({
+//               twitterId: profile.id,
+//               displayName: profile.name,
+//               avatar: profile.photos[0].value,
+//               provider: profile.provider,
+//             });
 
-            newUser.save ().then (user => done (null, user));
-          }
+//             newUser.save ().then (user => done (null, user));
+//           }
 
-          // user exists so no error (null) and pass in the user
-          done (null, existingUser);
-        })
-        .catch (err => done (err));
-    }
-  )
-);
+//           // user exists so no error (null) and pass in the user
+//           done (null, existingUser);
+//         })
+//         .catch (err => done (err));
+//     }
+//   )
+// );
